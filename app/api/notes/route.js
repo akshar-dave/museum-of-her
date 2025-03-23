ae6ed387378b51fa3b8a8fc43748f556da10712c
Akshar Dave <hello@akshardave.com>
2025-03-23 14:47:43 +0530

    created common supabase client

--- app/api/notes/route.js
+++ app/api/notes/route.js
@@ -1,11 +1,6 @@
 export const runtime = 'edge';
 
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+import { supabase } from "@/lib/supabaseClient";
 
 export async function GET() {
   try {
